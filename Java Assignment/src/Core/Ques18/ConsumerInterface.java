5cd6d6a5873ba208d1d56091cb1a443978cf9e20
Prakhar Rastogi <prakhar.rastogi@knoldus.com>
2021-10-25 16:51:19 +0530

    Document File Added

--- Java Assignment/src/Core/Ques18/ConsumerInterface.java
+++
@@ -1,7 +0,0 @@
-package Core.Ques18;
-
-public interface ConsumerInterface {
-    static void printMessage(String text){
-        System.out.println(text);
-    }
-}
--- Java Assignment/src/Core/Ques18/EmployeeFactory.java
+++
@@ -1,6 +0,0 @@
-package Core.Ques18;
-
-@FunctionalInterface
-public interface EmployeeFactory {
-    public abstract Employee getEmployee(String name, Integer age, String city);
-}
